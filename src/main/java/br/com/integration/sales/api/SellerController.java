ff092e2bf437b94ac2c0b41ddae353ca0e3e39f2
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-20 22:40:00 -0300

    rota post para criaçao de vendedor

--- src/main/java/br/com/integration/sales/api/SellerController.java
+++ src/main/java/br/com/integration/sales/api/SellerController.java
@@ -1,9 +1,16 @@
 package br.com.integration.sales.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +24,9 @@ public class SellerController {
     System.out.println("Hello, World!");
   }
 
+  @PostMapping("/seller")
+  public ResponseEntity<Seller> saveSeller(@RequestBody Seller seller) {
+    Seller savedSeller = repository.save(seller);
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedSeller);
+  }
 }
