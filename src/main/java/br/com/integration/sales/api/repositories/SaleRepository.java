f31bda6b242307745d8d3c5b1d59ed057917734f
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-22 02:07:42 -0300

    refatoraçao do repository da venda

--- src/main/java/br/com/integration/sales/api/repositories/SaleRepository.java
+++ src/main/java/br/com/integration/sales/api/repositories/SaleRepository.java
@@ -1,6 +1,8 @@
-package br.com.integration.sales.api;
+package br.com.integration.sales.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.integration.sales.api.models.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
