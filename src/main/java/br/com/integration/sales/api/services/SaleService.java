3ed7191c3fd9ec33789da75ddb82eff612d4ceaa
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-22 02:17:11 -0300

    criaçao do service da venda utlizado na rota post

---
+++ src/main/java/br/com/integration/sales/api/services/SaleService.java
@@ -0,0 +1,31 @@
+package br.com.integration.sales.api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.integration.sales.api.dtos.CreateSaleRequestDTO;
+import br.com.integration.sales.api.models.Sale;
+import br.com.integration.sales.api.models.Seller;
+import br.com.integration.sales.api.repositories.SaleRepository;
+import br.com.integration.sales.api.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+  @Autowired
+  SaleRepository saleRepository;
+
+  @Autowired
+  SellerRepository sellerRepository;
+
+  public Sale create(CreateSaleRequestDTO sale) {
+    Optional<Seller> existingSeller = sellerRepository.findById(sale.getSellerId());
+    if (!existingSeller.isPresent()) {
+      throw new Error("Seller does not exists");
+    }
+
+    return saleRepository.save(new Sale(sale.getId(), sale.getSaledAt(), sale.getSaleValue(), existingSeller.get()));
+  }
+}
