acfd84cfd85897b31397feddc8afaa23aad33ce7
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-23 13:15:35 -0300

    adiçao e mapeamento da query de busca do vendedor

--- src/main/java/br/com/integration/sales/api/models/Seller.java
+++ src/main/java/br/com/integration/sales/api/models/Seller.java
@@ -4,20 +4,33 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedNativeQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.SqlResultSetMapping;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.integration.sales.api.dtos.SellerResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+
+@NamedNativeQuery(name = "Seller.listSellersWithAvgAndSum_Named", query = "SELECT S.SELLER_NAME AS name,           (          SELECT          SUM(SALE_VALUE)          FROM SALE SA            WHERE SA.SELLER_ID = S.ID            GROUP BY SALED_AT,                      SELLER_ID                      FETCH FIRST 1 ROW ONLY ) AS averageDailySales,         (SELECT SUM(SALE_VALUE) FROM SALE WHERE SELLER_ID = S.ID)  as totalSalesAmount    FROM SELLER AS S", resultSetMapping = "Mapping.SellerResponseDTO")
+
+@SqlResultSetMapping(name = "Mapping.SellerResponseDTO", classes = @ConstructorResult(targetClass = SellerResponseDTO.class, columns = {
+    @ColumnResult(name = "name"),
+    @ColumnResult(name = "averageDailySales"),
+    @ColumnResult(name = "totalSalesAmount"),
+}))
 
 @Entity
 @Data
