e2cb3a4d314bbc4007216a5b895e8c9d323a15e4
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-23 13:16:30 -0300

    adiçao da query de busca do vendedor

--- src/main/java/br/com/integration/sales/api/repositories/SellerRepository.java
+++ src/main/java/br/com/integration/sales/api/repositories/SellerRepository.java
@@ -1,9 +1,16 @@
 package br.com.integration.sales.api.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.integration.sales.api.dtos.SellerResponseDTO;
 import br.com.integration.sales.api.models.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+  @Query(nativeQuery = true)
+  List<SellerResponseDTO> listSellersWithAvgAndSum_Named();
 
 }
