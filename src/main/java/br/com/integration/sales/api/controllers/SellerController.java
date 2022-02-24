7ee849a5b6d8b3b971559984ca2291b85f534442
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-24 13:41:43 -0300

    adiçao de busca por id

--- src/main/java/br/com/integration/sales/api/controllers/SellerController.java
+++ src/main/java/br/com/integration/sales/api/controllers/SellerController.java
@@ -1,18 +1,24 @@
 package br.com.integration.sales.api.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.integration.sales.api.dtos.SellerResponseDTO;
 import br.com.integration.sales.api.models.Seller;
-import br.com.integration.sales.api.repositories.SaleRepository;
+import br.com.integration.sales.api.repositories.SellerRepository;
 import br.com.integration.sales.api.services.SellerService;
 
 @RestController
@@ -22,7 +28,7 @@ public class SellerController {
   SellerService sellerService;
 
   @Autowired
-  SaleRepository saleRepository;
+  SellerRepository sellerRepository;
 
   @GetMapping("/hello")
   public void main(String[] args) {
@@ -40,5 +46,17 @@ public class SellerController {
     List<SellerResponseDTO> sellers = sellerService.listSellers();
     return ResponseEntity.status(HttpStatus.OK).body(sellers);
   }
+
+  @GetMapping("/seller/{id}")
+  public ResponseEntity<Seller> listById(@PathVariable Long id) {
+    Seller seller = sellerService.findById(id);
+    return ResponseEntity.status(HttpStatus.OK).body(seller);
+  }
+
+  // @DeleteMapping("/seller/{id}")
+  // @ResponseStatus(HttpStatus.NO_CONTENT)
+  // public void deletedSeller(@PathVariable Long id) {
+  // sellerService.deleteById(id);
+  // }
 
 }
