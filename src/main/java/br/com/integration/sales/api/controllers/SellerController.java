6480f8fe8fb6e62f7ab9e60b537f5e04395c467e
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-24 22:22:14 -0300

    refatoraçao da rota para utilizaçao das datas

--- src/main/java/br/com/integration/sales/api/controllers/SellerController.java
+++ src/main/java/br/com/integration/sales/api/controllers/SellerController.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.integration.sales.api.dtos.SellerResponseDTO;
@@ -47,7 +45,7 @@ public class SellerController {
       @RequestParam(name = "endDate", defaultValue = "2030-01-01") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate) {
     System.out.println(startDate);
     System.out.println(endDate);
-    List<SellerResponseDTO> sellers = sellerService.listSellers();
+    List<SellerResponseDTO> sellers = sellerService.listSellers(startDate, endDate);
     return ResponseEntity.status(HttpStatus.OK).body(sellers);
   }
 
