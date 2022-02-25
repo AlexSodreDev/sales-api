82ad577e21d48f9281baf8739ce5dae5c74bdf17
AlexSodreDev <alexandre.cortines@gmail.com>
2022-02-24 22:28:29 -0300

    adiçao de serviço utilizando o sellerJbdc

--- src/main/java/br/com/integration/sales/api/services/SellerService.java
+++ src/main/java/br/com/integration/sales/api/services/SellerService.java
@@ -1,13 +1,14 @@
 package br.com.integration.sales.api.services;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.integration.sales.api.dtos.SellerResponseDTO;
 import br.com.integration.sales.api.models.Seller;
+import br.com.integration.sales.api.repositories.SellerJdbcRepository;
 import br.com.integration.sales.api.repositories.SellerRepository;
 
 @Service
@@ -16,12 +17,20 @@ public class SellerService {
   @Autowired
   SellerRepository repository;
 
+  @Autowired
+  SellerJdbcRepository sellerJdbcRepository;
+
   public Seller create(Seller seller) {
     return repository.save(seller);
   }
 
   public List<SellerResponseDTO> listSellers() {
     List<SellerResponseDTO> sellers = repository.listSellersWithAvgAndSum_Named();
+    return sellers;
+  }
+
+  public List<SellerResponseDTO> listSellers(Date startDate, Date endDate) {
+    List<SellerResponseDTO> sellers = sellerJdbcRepository.listSellersWithAvgAndSum(startDate, endDate);
     return sellers;
   }
 
